12a97c3c8393c7669abe0682b879857b88cc6dbd
shubhamvmhaske <shubhammhaske@tamu.edu>
2023-09-24 17:18:30 -0500

    added movies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Scarface', :rating => 'R',
+    :release_date => '09-Dec-1983'}
 ]
 
 more_movies.each do |movie|
